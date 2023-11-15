9b053c9ef43aa6cb0dab3af9a224ed49e460ca80
Indo Krish <55687559+Indok93@users.noreply.github.com>
2023-11-15 14:57:57 +0100

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,29 @@
-/* lav funktion der ændre property display 
-til flex når burger-menu er klikket */
+
 const buttonBurger = document.getElementById('burgerMenu');
 
-buttonBurger.addEventListener('click', () => {
+buttonBurger.addEventListener('click', (e) => {
+    e.preventDefault();
     const navBar = document.getElementById('navigation');
+    const burgerMenu = document.querySelector('.burger-menu');
+    const cards = document.querySelector('.card-wrapper');
     navBar.style.display = 'flex';
+    burgerMenu.style.backgroundColor = 'transparent';
+    if (cards) {
+        cards.style.display = 'none';
+    }
+});
+
+const buttonSubmit = document.getElementById('submitButton');
+buttonSubmit.addEventListener('click', (e) => {
+    e.preventDefault();
+    // console.log(buttonSubmit);
+    buttonSubmit.style.width = 'calc(100% - 2px)';
+    buttonText = document.querySelector('.before-submit');
+    buttonText.innerText = 'Subscribed';
+
 });
 
 function changeBackground(url) {
-    document.querySelector('header').style.backgroundImage = `url('${url}')`;
+    const headerBackground = document.querySelector('header');
+    headerBackground.style.backgroundImage = `url('${url}')`;
 }
-
